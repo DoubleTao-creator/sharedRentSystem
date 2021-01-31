f7e42ad7577677f8cc6c003e735b2dfc647cf8c8
徐一婵 <1830069482@qq.com>
2021-01-31 16:58:08 +0800

    7

--- cgoods-provider8008/src/main/java/com/xyc/service/CGoodsService.java
+++ cgoods-provider8008/src/main/java/com/xyc/service/CGoodsService.java
@@ -2,21 +2,24 @@ package com.xyc.service;
 
 import com.xyc.dto.CGoodsAddDTO;
 import com.xyc.dto.CGoodsModifyDTO;
+import com.xyc.dto.CGoodsShowDTO;
 import com.xyc.pojo.CGoods;
 
 import java.util.List;
 
 public interface CGoodsService {
 
+    public CGoodsShowDTO getById(Integer id);
+
     public int add(CGoodsAddDTO cGoodsAD);
 
-    public List<CGoods> getAll();
+    public List<CGoodsShowDTO> getAll();
 
-    public List<CGoods> searchBySellerId(Integer sellerId);
+    public List<CGoodsShowDTO> searchBySellerId(Integer sellerId);
 
-    public List<CGoods> fuzzySearch(String info);
+    public List<CGoodsShowDTO> fuzzySearch(String info);
 
-    public List<CGoods> searchByTypeId(Integer typeId);
+    public List<CGoodsShowDTO> searchByTypeId(Integer typeId);
 
     public int updateInfo(CGoodsModifyDTO cGoodsAD);
 
